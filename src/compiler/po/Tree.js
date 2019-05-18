48933045f4737707bd5e9bab3040f7828f6ed769
coder-zzk <2238109333@qq.com>
2019-05-18 15:47:22 +0800

    add 添加先序遍历和后序遍历函数 #Tree

--- src/compiler/po/Tree.js
+++ src/compiler/po/Tree.js
@@ -2,11 +2,43 @@ let Token = require("./Token");
 /**
  * 树
  * @param {*} val 根节点值
- * @param {Array<Tree>}  子节点数组
+ * @param {Array<Tree>} children 子节点数组
  */
-function Tree(val, nodes = []) {
-  this.val = val;
-  if (!(nodes instanceof Array)) throw new Error("非法的子节点值:nodes");
-  this.nodes = nodes;
+function Tree(name, children = []) {
+  this.name = name;
+  if (!(children instanceof Array)) throw new Error("非法的子节点值:children");
+  this.children = children;
+  this.lastRootAccess = lastRootAccess;
+  this.firstRootAccess = firstRootAccess;
+}
+/**
+ * 后续遍历
+ * @param {Tree} tree
+ */
+function lastRootAccess(tree) {
+  let result = [];
+  if (!(tree instanceof Tree)) throw new Error("非法的参数:tree");
+  tree.children.map(t => {
+    lastRootAccess(t).map(tt => {
+      result.push(tt);
+    });
+  });
+  result.push(tree);
+  return result;
+}
+/**
+ * 先序遍历
+ * @param {Tree} tree
+ */
+function firstRootAccess(tree) {
+  let result = [];
+  if (!(tree instanceof Tree)) throw new Error("非法的参数:tree");
+  result.push(tree);
+  tree.children.map(t => {
+    lastRootAccess(t).map(tt => {
+      result.push(tt);
+    });
+  });
+  return result;
 }
 module.exports = Tree;
